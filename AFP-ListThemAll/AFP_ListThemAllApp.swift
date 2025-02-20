6a491b0abe918551e8dc3bf5bbc1541116ca3921
Donovan <donovanongty@gmail.com>
2025-02-21 10:31:18 +1100

    Initial Commit

---
+++ AFP-ListThemAll/AFP_ListThemAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFP_ListThemAllApp.swift
+//  AFP-ListThemAll
+//
+//  Created by Donovan Ong on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct AFP_ListThemAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
